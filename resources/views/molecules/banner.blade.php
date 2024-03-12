51b32663ea17b5f016245ea199f9a726a11a98b2
Juno Nguyen <juno.nguyen@kyberosc.com>
2024-03-13 01:25:16 +0700

    create banner and refactor code

---
+++ resources/views/molecules/banner.blade.php
@@ -0,0 +1,6 @@
+<div class="banner">
+  <div class="container">
+    <div class="banner__title">TUD MEDIA Welcomes You</div>
+    <a href="#contact" class="banner__button">Contact</a>
+  </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,21 +3,34 @@
 @section('title', 'TUD Media')
 
 @section('style-libraries')
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.5/swiper-bundle.min.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/css/bootstrap.min.css"/>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
 @stop
 
 @section('styles')
-    {{--custom css item suggest search--}}
-    <style>
 
-    </style>
+{{--custom css item suggest search--}}
+<style>
+  @font-face {
+    font-family: "Roboto Slab", serif;
+  }
+</style>
 @stop
 
 @section('content')
-    <h1>Homepage</h1>
+@include('molecules.banner')
+
 @stop
 
 @section('scripts')
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.5/swiper-bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/js/bootstrap.min.js"></script>
 @stop
 <!-- /find-tickets?adt=1&chd=3&inf=0&from=HAN&to=SGN&depart=20032024 -->
